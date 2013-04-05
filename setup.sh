66ef8850dc0aa0084df1e61e7c7523f56fbaab89
Andrej Lamov <andrej.lamov@gmail.com>
2013-04-05 11:42:35 +0200

    Comment the dirname and readlink line

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #Make the binaries in target visible
 
+#Source this file from any position and get correct target-path
 TARGET_BIN=$(dirname $(readlink -f $0))/target/pkg/bin
 
 export PATH=$TARGET_BIN:$PATH
