8f6cb8a79c3ade299031c227c181e616dabc7875
Andrej Lamov <andrej.lamov@gmail.com>
2013-04-05 12:10:02 +0200

    Fetch list with package vulnerabilities

--- boot.sh
+++ boot.sh
@@ -10,4 +10,6 @@ rm pkgsrc.tar.gz
 #TARGET is the main dir where packages will be installed 
 ./pkgsrc/bootstrap/bootstrap --unprivileged --prefix=$TARGET/pkg --pkgdbdir=$TARGET/db/pkg --sysconfdir=$TARGET/etc --varbase=$TARGET/var
 
+target/pkg/sbin/pkg_admin -K target/db/pkg fetch-pkg-vulnerabilities
+
 source setup.sh
