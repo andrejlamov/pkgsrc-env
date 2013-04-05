c5269e858cc32eb808dc49d87db4c14277c1a041
Andrej Lamov <andrej.lamov@gmail.com>
2013-04-05 09:42:48 +0200

    Rephrase comment

--- boot.sh
+++ boot.sh
@@ -6,6 +6,6 @@ export SH=/bin/bash #shell must be BSD-compatible
 wget http://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc.tar.gz
 tar xzvf pkgsrc.tar.gz
 rm pkgsrc.tar.gz
-#let pkgsrc live in the TARGET-directory 
+#TARGET is the main dir where packages will be installed 
 ./pkgsrc/bootstrap/bootstrap --unprivileged --prefix=$TARGET/pkg --pkgdbdir=$TARGET/db/pkg --sysconfdir=$TARGET/etc --varbase=$TARGET/var
 
