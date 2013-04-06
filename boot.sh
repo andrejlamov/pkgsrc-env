af8baf47b087fc93fac5de8a3fa859708677abce
Andrej Lamov <andrej.lamov@gmail.com>
2013-04-06 21:21:52 +0200

    Genereate setup.sh in boot script
    Keep setup.sh outside of repo so that it can be dynamically modified

--- boot.sh
+++ boot.sh
@@ -12,3 +12,10 @@ rm pkgsrc.tar.gz
 
 target/pkg/sbin/pkg_admin -K target/db/pkg fetch-pkg-vulnerabilities
 
+#Generate initial setup that can be sourced
+SETUP=`pwd`/setup.sh
+echo '#!/bin/bash'                                               >> $SETUP
+echo 'TARGET=$(dirname $(readlink -f ${BASH_SOURCE[0]}))/target' >> $SETUP
+echo 'TARGET_BIN=$TARGET/pkg/bin'                                >> $SETUP
+echo 'TARGET_SBIN=$TARGET/pkg/sbin'                              >> $SETUP
+echo 'export PATH=$TARGET_BIN:$TARGET_SBIN:$PATH'                >> $SETUP
